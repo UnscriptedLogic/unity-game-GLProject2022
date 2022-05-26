e3b797823b734fd619a6f38297c06ea2510d11f3
Nathaniel <semblancewarden@Gmail.com>
2022-05-26 17:25:58 +0800

    Revert "Object Pooling for projectiles"
    This reverts commit 75f0ae580cd2d0823c3e0d8c41745dc94c04dfca.

--- Assets/_Scripts/Projectiles/Projectile.cs
+++ Assets/_Scripts/Projectiles/Projectile.cs
@@ -10,29 +10,21 @@ namespace Projectiles
     public class Projectile : MonoBehaviour
     {
         [SerializeField] private float movementSpeed = 10f;
-        [SerializeField] private float lifeTime = 1f;
         [SerializeField] private Rigidbody rb;
-
-        private float _lifetime;
         private PoolManager poolManager;
-        private MovementBehaviour movementBehaviour;
 
         public PoolManager PoolManager { get => poolManager; set { poolManager = value; } }
 
-        private void OnEnable()
+        private MovementBehaviour movementBehaviour;
+
+        private void Start()
         {
-            _lifetime = lifeTime;
             movementBehaviour = new MovementBehaviour();
         }
 
         private void Update()
         {
             movementBehaviour.MoveEntity(movementSpeed, transform.forward, rb);
-
-            if (_lifetime <= 0)
-                poolManager.PushToPool(gameObject);
-            else
-                _lifetime -= Time.deltaTime;
         }
 
         private void OnTriggerEnter(Collider other)

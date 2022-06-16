7578c4ee3fda7b617e6d993b5471a15d409faf2c
Nathaniel <semblancewarden@Gmail.com>
2022-06-16 20:59:37 +0800

    Tower LoadOuts and Wave Changes

--- Assets/_Scripts/Game/LoadOutManager.cs
+++ Assets/_Scripts/Game/LoadOutManager.cs
@@ -9,21 +9,6 @@ namespace Game
     {
         [SerializeField] private static List<TowerSO> selectedTowers = new List<TowerSO>();
 
-        public static List<TowerSO> SelectedTowers => selectedTowers;
-
-        public static void AddToLoadOut(TowerSO tower)
-        {
-            selectedTowers.Add(tower);
-        }
-
-        public static void RemoveFromLoadOut(TowerSO tower)
-        {
-            selectedTowers.Remove(tower);
-        }
-
-        public static void RemoveLast()
-        {
-            selectedTowers.RemoveAt(selectedTowers.Count - 1);
-        }
+        public static List<TowerSO> SelectedTowers { get => selectedTowers; set { selectedTowers = value; } }
     }
 }
---
+++ Assets/_Scripts/UI/TowerMenu.cs
@@ -0,0 +1,144 @@
+using Game;
+using Standalone;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Towers;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MainMenu.UI
+{
+    public class TowerMenu : MonoBehaviour
+    {
+        [Header("Load Out")]
+        [SerializeField] private int maxLoadOut = 5;
+        [SerializeField] private Transform loadoutList;
+
+        [Header("Tower List")]
+        [SerializeField] private Transform towerList;
+        [SerializeField] private GameObject buttonPrefab;
+        [SerializeField] private TowerSO[] towers;
+
+        [Header("Details")]
+        [SerializeField] private Image icon;
+        [SerializeField] private TextMeshProUGUI towerNameTMP;
+        
+        [SerializeField] private TextMeshProUGUI damageTMP;
+        [SerializeField] private TextMeshProUGUI rangeTMP;
+        [SerializeField] private TextMeshProUGUI fireRateTMP;
+        [SerializeField] private TextMeshProUGUI projSpeedTMP;
+        [SerializeField] private TextMeshProUGUI currencyTMP;
+        [SerializeField] private TextMeshProUGUI upgradesTMP;
+
+        private List<TowerSO> towerLoadOut;
+
+        private void Start()
+        {
+            if (LoadOutManager.SelectedTowers.Count > 0)
+                towerLoadOut = LoadOutManager.SelectedTowers;
+            else
+                towerLoadOut = new List<TowerSO>();
+
+            DisplayTowers();
+            UpdateLoadOut();
+        }
+
+        public void DisplayDetails(TowerSO towerSO)
+        {
+            TowerDetails towerDetails = towerSO.TreeList[0];
+            icon.sprite = towerDetails.Icon;
+            towerNameTMP.text = towerDetails.Name;
+
+            Tower tower = towerSO.BaseTower.GetComponent<Tower>();
+            damageTMP.text = tower.Damage.ToString();
+            rangeTMP.text = tower.Range.ToString();
+            fireRateTMP.text = tower.FireRate.ToString();
+            projSpeedTMP.text = tower.ProjSpeed.ToString();
+            currencyTMP.text = towerSO.TowerCost.ToString();
+            upgradesTMP.text = towerSO.TreeList.Length.ToString();
+        }
+
+        public void DisplayTowers()
+        {
+            for (int i = 0; i < towerList.childCount; i++)
+            {
+                Destroy(towerList.GetChild(i).gameObject);
+            }
+
+            TowerSO towerUpgradeTree;
+            for (int i = 0; i < towers.Length; i++)
+            {
+                GameObject towerButton = Instantiate(buttonPrefab, towerList);
+                towerButton.transform.GetChild(0).GetComponent<Image>().sprite = towers[i].TreeList[0].Icon;
+
+                towerUpgradeTree = towers[i];
+                AddListener(towerButton.GetComponent<Button>(), towerUpgradeTree, tower =>
+                {
+                    if (towerNameTMP.text == tower.TreeList[0].Name)
+                        AddToLoadOut(tower);
+
+                    DisplayDetails(tower);
+                });
+            }
+        }
+
+        private void AddListener(Button button, TowerSO tower, Action<TowerSO> method)
+        {
+            TowerSO towerSO = tower;
+            button.onClick.AddListener(() =>
+            {
+                method(tower);
+            });
+        }
+
+        public void UpdateLoadOut()
+        {
+            for (int i = 0; i < loadoutList.childCount; i++)
+            {
+                loadoutList.GetChild(i).GetChild(1).GetComponent<Image>().enabled = false;
+            }
+
+            for (int i = 0; i < towerLoadOut.Count; i++)
+            {
+                Image image = loadoutList.GetChild(i).GetChild(1).GetComponent<Image>();
+                image.sprite = towerLoadOut[i].TreeList[0].Icon;
+                image.enabled = true;
+
+                Button button = loadoutList.GetChild(i).GetComponent<Button>();
+                button.onClick.RemoveAllListeners();
+
+                AddListener(button, towerLoadOut[i], tower =>
+                {
+                    RemoveFromLoadOut(tower);
+                });
+            }
+        }
+
+        public void AddToLoadOut(TowerSO towerSO)
+        {
+            if (towerLoadOut.Count < maxLoadOut)
+            {
+                if (!towerLoadOut.Contains(towerSO))
+                {
+                    towerLoadOut.Add(towerSO);
+                }
+            }
+
+            LoadOutManager.SelectedTowers = towerLoadOut;
+            UpdateLoadOut();
+        }
+
+        public void RemoveFromLoadOut(TowerSO tower)
+        {
+            if (towerLoadOut.Contains(tower))
+            {
+                towerLoadOut.Remove(tower);
+            }
+
+            LoadOutManager.SelectedTowers = towerLoadOut;
+            UpdateLoadOut();
+        }
+    }
+}

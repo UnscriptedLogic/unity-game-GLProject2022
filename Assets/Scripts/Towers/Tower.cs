336755dae33c4fb8cff59359d3e29052e2241568
Nathaniel Alcala <semblancewarden@Gmail.com>
2023-12-09 16:22:20 +0800

    Fixed targetting system

--- Assets/Scripts/Towers/Tower.cs
+++ Assets/Scripts/Towers/Tower.cs
@@ -7,18 +7,29 @@ using Units;
 using Projectiles;
 using Core.Pooling;
 using Core.Grid;
+using System.Linq;
 
 namespace Towers
 {
     public class Tower : MonoBehaviour, IFocuseable, IAttackable
     {
+        public enum TargettingMode
+        {
+            First,
+            Last,
+            Closest,
+            Furthest,
+            Weakest,
+            Strongest
+        }
+
         [Header("Attributes")]
         [SerializeField] protected int towerID = 0;
         [SerializeField] protected float damage = 1f;
         [SerializeField] protected float projectileSpeed = 30f;
         [SerializeField] protected float projectileLifetime = 0.5f;
         [SerializeField] protected float piercingPercent = 0f;
-
+        [SerializeField] protected TargettingMode targettingMode = TargettingMode.First;
         [SerializeField] protected float range = 5f;
         [SerializeField] protected float attackInterval = 1f;
         [SerializeField] protected float rotationSpeed = 5f;
@@ -26,8 +37,9 @@ namespace Towers
 
         protected float _attackInterval;
         protected Collider[] unitsInRange;
+        protected List<EnemyUnits> targetsInRange = new List<EnemyUnits>();
 
-        protected Unit target;
+        protected Unit currentTarget;
         protected AttackBehaviour attackBehaviour;
 
         [Header("Others")]
@@ -59,62 +71,109 @@ namespace Towers
             attackBehaviour = new AttackBehaviour(PoolManager.instance);
         }
 
+        protected virtual void FixedUpdate()
+        {
+            if (currentTarget != null)
+            {
+                if (LostTarget())
+                {
+                    currentTarget = null;
+                    return;
+                }
+            }
+
+            if (targetsInRange.Count > 0)
+            {
+                targetsInRange.Clear();
+            }
+
+            Vector3 adjustedCenter = new Vector3(transform.position.x, 0.2f, transform.position.z);
+            unitsInRange = Physics.OverlapSphere(adjustedCenter, range, unitLayer);
+            foreach (Collider collider1 in unitsInRange)
+            {
+                EnemyUnits unitMovement = collider1.GetComponent<EnemyUnits>();
+                if (unitMovement == null) continue;
+
+                if (Vector3.Distance(unitMovement.transform.position, adjustedCenter) > range || unitMovement.WaypointIndex == 0) continue;
+
+                targetsInRange.Add(unitMovement);
+            }
+
+            if (targetsInRange.Count > 0)
+            {
+                SortTargets();
+            }
+        }
+
+        protected void SortTargets()
+        {
+            EnemyUnits target = null;
+
+            switch (targettingMode)
+            {
+                case TargettingMode.First:
+                    //Node closest to the end point
+                    target = targetsInRange.OrderByDescending(t => t.GetComponent<EnemyUnits>().WaypointIndex).FirstOrDefault();
+                    break;
+                case TargettingMode.Last:
+                    //Node furthest from the end point
+                    target = targetsInRange.OrderBy(t => t.GetComponent<EnemyUnits>().WaypointIndex).FirstOrDefault();
+                    break;
+                case TargettingMode.Closest:
+                    //Closest distance to tower
+                    target = targetsInRange.OrderBy(t => Vector3.Distance(t.transform.position, transform.position)).FirstOrDefault();
+                    break;
+                case TargettingMode.Furthest:
+                    //Furthest distance to tower
+                    target = targetsInRange.OrderByDescending(t => Vector3.Distance(t.transform.position, transform.position)).FirstOrDefault();
+                    break;
+                case TargettingMode.Strongest:
+                    //Highest health point in range
+                    target = targetsInRange.OrderByDescending(t => t.GetComponent<EnemyUnits>().CurrentHealth).FirstOrDefault();
+                    break;
+                case TargettingMode.Weakest:
+                    //Lowest health point in range
+                    target = targetsInRange.OrderBy(t => t.GetComponent<EnemyUnits>().CurrentHealth).FirstOrDefault();
+                    break;
+                default:
+                    break;
+            }
+
+            if (currentTarget == null || target != currentTarget)
+            {
+                currentTarget = target;
+            }
+        }
+
         protected virtual void Update()
         {
-            if (_attackInterval >= 0)
+            if (_attackInterval >= 0f)
             {
                 _attackInterval -= Time.deltaTime;
             }
 
-            if (_attackInterval <= 0f)
+            if (_attackInterval > 0f) return;
+
+            if (currentTarget == null) return;
+
+            FocusObject(rotationPart, currentTarget.transform.position, rotationSpeed);
+            DoAttack(projectilePrefab, shootAnchor);
+            _attackInterval = attackInterval;
+        }
+
+        protected bool LostTarget()
+        {
+            if (!currentTarget.gameObject.activeInHierarchy)
             {
-                Vector3 adjustedCenter = new Vector3(transform.position.x, 0.2f, transform.position.z);
-                unitsInRange = Physics.OverlapSphere(adjustedCenter, range, unitLayer);
-                foreach (Collider collider1 in unitsInRange)
-                {
-                    EnemyUnits unitMovement = collider1.GetComponent<EnemyUnits>();
-                    if (unitMovement != null)
-                    {
-                        if (target == null)
-                        {
-                            target = unitsInRange[0].GetComponent<EnemyUnits>();
-                        }
-                        else
-                        {
-                            if (Vector3.Distance(target.transform.position, adjustedCenter) > range || target.WaypointIndex == 0)
-                            {
-                                if (unitsInRange.Length > 1)
-                                {
-                                    target = unitsInRange[1].GetComponent<EnemyUnits>();
-                                }
-                                else
-                                {
-                                    target = null;
-                                }
-                                return;
-                            }
-
-                            //Switch targets if one is ahead of the other
-                            if (unitMovement.WaypointIndex > target.WaypointIndex)
-                                target = unitMovement;
-                        }   
-                    }
-                }
-
-                if (unitsInRange.Length > 0 && target != null)
-                {
-                    //Vector3 correctedOrigin = new Vector3(transform.position.x, shootAnchor.position.y, transform.position.z);
-                    //Ray ray = new Ray(correctedOrigin, target.transform.position - correctedOrigin);
-                    //if (!Physics.Raycast(ray, range, wallLayer))
-                    //{
-
-                    //}
-
-                    FocusObject(rotationPart, target.transform.position, rotationSpeed);
-                    DoAttack(projectilePrefab, shootAnchor);
-                    _attackInterval = attackInterval;
-                }
+                return true;
             }
+
+            if (Vector3.Distance(currentTarget.transform.position, transform.position) >= range + 1f)
+            {
+                return true;
+            }
+
+            return false;
         }
 
         protected virtual void OnDrawGizmos()

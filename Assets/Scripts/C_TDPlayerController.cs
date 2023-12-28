79763b8f4a42505a06bb65cb85bb233e00043a44
Nathaniel Alcala <semblancewarden@Gmail.com>
2023-12-29 00:22:32 +0800

    Camera Zooming Works

--- Assets/Scripts/C_TDPlayerController.cs
+++ Assets/Scripts/C_TDPlayerController.cs
@@ -14,8 +14,15 @@ public class C_TDPlayerController : UController
         levelManager = GameMode.CastTo<GM_TowerDefenceGameMode>();
 
         defaultMap = levelManager.InputContext.FindActionMap("Default");
+
+        defaultMap.FindAction("MouseScroll").performed += C_TDPlayerController_performed;
      
         base.OnLevelStarted();
+    }
+
+    private void C_TDPlayerController_performed(InputAction.CallbackContext obj)
+    {
+        playerPawn.ZoomCamera(obj.ReadValue<float>());
     }
 
     protected override ULevelPawn PossessPawn()
@@ -31,5 +38,12 @@ public class C_TDPlayerController : UController
         mousePos = defaultMap.FindAction("MousePosition").ReadValue<Vector2>();
 
         playerPawn.MovePlayerCamera(mousePos);
+    }
+
+    protected override void OnDestroy()
+    {
+        defaultMap.FindAction("MouseScroll").performed -= C_TDPlayerController_performed;
+
+        base.OnDestroy();
     }
 }
--- Assets/Scripts/P_TDPlayerPawn.cs
+++ Assets/Scripts/P_TDPlayerPawn.cs
@@ -3,7 +3,13 @@ using UnscriptedEngine;
 
 public class P_TDPlayerPawn : URTSCamera
 {
+    [Header("Extension")]
     [SerializeField] private Vector2 panningDetectionThickness = new Vector2(5, 5);
+
+    [Header("Scrolling")]
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private float scrollSpeed = 1f;
+    [SerializeField] private Vector2 scrollBounds;
 
     public void MovePlayerCamera(Vector2 mousePos)
     {
@@ -25,6 +31,17 @@ public class P_TDPlayerPawn : URTSCamera
         if (mousePos.y <= panningDetectionThickness.y)
         {
             MoveCamera(Direction.Backward);
+        }
+    }
+
+    public void ZoomCamera(float value)
+    {
+        Vector3 prevPos = cameraTransform.position;
+        cameraTransform.Translate(cameraTransform.forward * value * (scrollSpeed * 100f) * Time.deltaTime, Space.World);
+
+        if (cameraTransform.position.y <= scrollBounds.x || cameraTransform.position.y >= scrollBounds.y)
+        {
+            cameraTransform.position = prevPos;
         }
     }
 }

7b779e28987e173210ac7b2ec2e2c3819dda1fda
Nathaniel Alcala <semblancewarden@Gmail.com>
2023-12-29 01:11:35 +0800

    Pausing Works

--- Assets/Scripts/UIC_GameHUD.cs
+++ Assets/Scripts/UIC_GameHUD.cs
@@ -1,7 +1,31 @@
+using System;
 using UnityEngine;
 using UnscriptedEngine;
 
 public class UIC_GameHUD : UCanvasController
 {
-    // Your script code here
+    [SerializeField] private UIC_PauseMenu pauseUIPrefab;
+
+    private ULevelObject context;
+
+    public override void OnWidgetAttached(ULevelObject context)
+    {
+        base.OnWidgetAttached(context);
+
+        this.context = context;
+
+        Bind<UButtonComponent>("Pause", OnPause);
+    }
+
+    private void OnPause()
+    {
+        context.AttachUIWidget(pauseUIPrefab);
+    }
+
+    public override void OnWidgetDetached(ULevelObject context)
+    {
+        UnBind<UButtonComponent>("Pause", OnPause);
+
+        base.OnWidgetDetached(context);
+    }
 }
--- Assets/Scripts/UIC_PauseMenu.cs
+++ Assets/Scripts/UIC_PauseMenu.cs
@@ -1,7 +1,29 @@
+using System;
 using UnityEngine;
 using UnscriptedEngine;
 
 public class UIC_PauseMenu : UCanvasController
 {
-    // Your script code here
+    public override void OnWidgetAttached(ULevelObject context)
+    {
+        base.OnWidgetAttached(context);
+
+        GameMode.PauseGame();
+
+        Bind<UButtonComponent>("Resume", OnResume);
+    }
+
+    private void OnResume()
+    {
+        DettachUIWidget(this);
+    }
+
+    public override void OnWidgetDetached(ULevelObject context)
+    {
+        GameMode.ResumeGame();
+
+        base.OnWidgetDetached(context);
+
+        Destroy(gameObject);
+    }
 }
